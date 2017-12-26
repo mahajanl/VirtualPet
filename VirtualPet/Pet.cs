844b2aed494445b87ab1905cd4853b77eaf49f0c
mahajanl <lakshamima@gmail.com>
2017-12-26 15:34:05 -0500

    primary commit, redoing c# version. abstract class created and fields set; no constructors

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public abstract class Pet
+    {
+        public string name;
+        public int age;
+        public string type;
+        public bool hasFur;
+        public bool breathesAir;
+
+        public string Name
+        { get; set; }
+
+        public int Age
+        { get; set; }
+
+        public string Type
+        { get; set; }
+
+        public bool HasFur
+        { get; set; }
+
+        public bool BreathesAir
+        { get; set; }
+
+
+    }
+}
